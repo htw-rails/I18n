222e4b75bef32d3a7a0fa777d99136d718da8eee
B Kleinen <kleinen@infrastructure.de>
2012-06-12 17:02:36 +0200

    created new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShoppingList::Application.config.secret_token = '78f537d2c5f3a90dd228ba9341f029fa13744413a16ad330ea1608033b0cf345ceb3f4c7bf78550e54f10f486dea90d78c6b5fb96413dbfcb82557d8fd52517a'
